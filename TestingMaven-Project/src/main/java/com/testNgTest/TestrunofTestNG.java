c2430314761bb26a027c02630b593bc81d525dbd
RKSony <RKSony@RKSony-PC>
2018-06-22 16:52:34 +0530

    updated pom and Testrunclass

--- TestingMaven-Project/src/main/java/com/testNgTest/TestrunofTestNG.java
+++ TestingMaven-Project/src/main/java/com/testNgTest/TestrunofTestNG.java
@@ -1,10 +1,10 @@
 package com.testNgTest;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 
 public class TestrunofTestNG {
