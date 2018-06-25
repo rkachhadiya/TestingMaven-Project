5623a5ff7ed69ecbb5518ce0244c9172587d0a8c
RKComputer <RKComputer@RKComputer-PC>
2018-06-25 21:16:06 +0530

    Commiting branch RKComputer 2306_01

--- TestingMaven-Project/src/main/java/com/testNgTest/C1_RKSony.java
+++ TestingMaven-Project/src/main/java/com/testNgTest/C1_RKSony.java
@@ -7,12 +7,12 @@ public class C1_RKSony {
 	
 	@BeforeTest
 	public void setUpBrowser(){
-		System.out.println("This is code from rk sony computer");
+		System.out.println("This line has been updated by RKComputer2306_1 branch");
 	}
 	
 	@Test
 	public void testing(){
-		System.out.println("Created test by RKsony in Branch 2306");
+		System.out.println("Updated by RKComputer under branch 2306_1");
 	}
 
 }
